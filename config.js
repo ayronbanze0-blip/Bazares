b341ae90cff66ff27dca0fb91c7caabad14295d3
ayronbanze0-blip <ayronbanze0@gmail.com>
2026-06-23 11:35:01 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
    Altere BAZARES_API_BASE se o seu backend correr noutro endereço.
    Por defeito assume http://localhost:3001 (porta padrão do backend).
 ============================================================ */
-window.BAZARES_API_BASE = 'http://localhost:3001';
+window.BAZARES_API_BASE = 'https://bazare-s-production.up.railway.app';
